0db2861eb20511143bafadd18f09fe1d97eaa3ca
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2022-06-29 20:16:50 +0200

    Layed out the structure for the App
    still need to finish the AddRecipe Method

--- Recipe-Console-App/Program.cs
+++ Recipe-Console-App/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Recipe_Console_App;
+using Spectre.Console;
+using System.Text.Json;
+
+void AddRecipe()
+{
+	var title = AnsiConsole.Ask<string>("What is the [green]recipe[/] called?");
+	var recipe = new Recipe();
+	recipe.Title = title;
+
+	AnsiConsole.WriteLine("Enter all the [green]ingredients[/]. Once done, leave the ingredient field [red]empty[/] and press enter");
+	var ingredient = AnsiConsole.Ask<string>("Enter ingredient: ");
+	while (ingredient != "")
+	{
+		recipe.Ingredients.Add(ingredient);
+		ingredient = AnsiConsole.Prompt(new TextPrompt<string>("Enter ingredient: ").AllowEmpty());
+	};
+
+	AnsiConsole.WriteLine("Enter all the [green]instructions[/]. Once done, leave the instruction field [red]empty[/] and press enter");
+	var instruction = AnsiConsole.Ask<string>("Enter instruction: ");
+	while (instruction != "")
+	{
+		recipe.Instructions.Add(instruction);
+		instruction = AnsiConsole.Prompt(new TextPrompt<string>("Enter instruction: ").AllowEmpty());
+	};
+
+	recipe.Ingredients.ForEach(Console.WriteLine);
+	Console.WriteLine(recipe.Ingredients.Count);
+};
---
+++ Recipe-Console-App/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipe_Console_App
+{
+	internal class Recipe
+	{
+		public String? Title { get; set; }
+		public List<string> Ingredients { get; set; } = new List<string>();
+		public List<string> Instructions { get; set; } = new List<string>();
+		// temporarily making treating categories as strings
+		// will change later to a more robust type like class or enum
+		public List<string> Categories { get; set; } = new List<string>();
+	}
+}
